d29da8ed45506490f84b6d381a35b6a8c2206136
Alex Galindo <>
2024-08-01 01:40:51 -0600

    primer version

--- src/Generators/ApiDocumenter.php
+++ src/Generators/ApiDocumenter.php
@@ -2,19 +2,19 @@
 
 namespace Elalecs\LaravelDocumenter\Generators;
 
-use Illuminate\Support\Facades\Route;
+use PhpParser\Node;
+use PhpParser\NodeFinder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
-use ReflectionMethod;
-use ReflectionParameter;
 
-class ApiDocumenter
+class ApiDocumenter extends BasePhpParserDocumenter
 {
     protected $config;
     protected $documentation = [];
 
     public function __construct($config)
     {
+        parent::__construct();
         $this->config = $config;
     }
 
@@ -43,98 +43,158 @@ class ApiDocumenter
 
     protected function documentRouteFile($file)
     {
+        $ast = $this->parseFile($file);
+        $nodeFinder = new NodeFinder;
+
+        $routeNodes = $nodeFinder->find($ast, function(Node $node) {
+            return $node instanceof Node\Expr\MethodCall && 
+                   $node->name->name === 'group' &&
+                   $node->var->name->parts[0] === 'Route';
+        });
+
         $fileName = basename($file);
         $this->documentation[$fileName] = [];
 
-        // Load the routes
-        require $file;
+        foreach ($routeNodes as $routeNode) {
+            $this->processRouteGroup($routeNode, $fileName);
+        }
+    }
 
-        $routes = Route::getRoutes();
+    protected function processRouteGroup(Node\Expr\MethodCall $node, $fileName, $prefix = '')
+    {
+        $groupAttributes = $this->getGroupAttributes($node);
+        $newPrefix = $prefix . ($groupAttributes['prefix'] ?? '');
 
-        foreach ($routes as $route) {
-            if ($this->isApiRoute($route)) {
-                $this->documentRoute($fileName, $route);
+        foreach ($node->args[1]->value->stmts as $stmt) {
+            if ($stmt instanceof Node\Expr\MethodCall && $stmt->var->name->parts[0] === 'Route') {
+                $this->processRoute($stmt, $fileName, $newPrefix, $groupAttributes['middleware'] ?? []);
+            } elseif ($stmt instanceof Node\Expr\MethodCall && $stmt->name->name === 'group') {
+                $this->processRouteGroup($stmt, $fileName, $newPrefix);
             }
         }
     }
 
-    protected function isApiRoute($route)
+    protected function getGroupAttributes(Node\Expr\MethodCall $node)
     {
-        // You might want to adjust this logic based on your API route naming conventions
-        return Str::startsWith($route->uri(), 'api/');
+        $attributes = [];
+        if ($node->args[0]->value instanceof Node\Expr\Array_) {
+            foreach ($node->args[0]->value->items as $item) {
+                if ($item->key->value === 'prefix') {
+                    $attributes['prefix'] = $item->value->value;
+                } elseif ($item->key->value === 'middleware') {
+                    $attributes['middleware'] = $this->extractMiddleware($item->value);
+                }
+            }
+        }
+        return $attributes;
     }
 
-    protected function documentRoute($fileName, $route)
+    protected function processRoute(Node\Expr\MethodCall $node, $fileName, $prefix, $groupMiddleware)
     {
-        $methods = $route->methods();
-        $uri = $route->uri();
-        $action = $route->getAction();
-
-        $handlerClass = $action['controller'] ?? null;
-        if ($handlerClass) {
-            list($class, $method) = explode('@', $handlerClass);
-        } else {
-            $class = null;
-            $method = null;
-        }
+        $method = $node->name->name;
+        $uri = $prefix . '/' . $node->args[0]->value->value;
+        $action = $this->getRouteAction($node->args[1]->value);
+        $middleware = array_merge($groupMiddleware, $this->getRouteMiddleware($node));
 
         $this->documentation[$fileName][] = [
-            'methods' => $methods,
+            'method' => strtoupper($method),
             'uri' => $uri,
-            'handlerClass' => $class,
-            'handlerMethod' => $method,
-            'middleware' => $this->getMiddleware($route),
-            'parameters' => $this->getParameters($class, $method),
-            'description' => $this->getMethodDescription($class, $method),
+            'action' => $action,
+            'middleware' => $middleware,
+            'parameters' => $this->getActionParameters($action),
+            'description' => $this->getActionDescription($action),
         ];
     }
 
-    protected function getMiddleware($route)
+    protected function getRouteAction($node)
     {
-        return collect($route->gatherMiddleware())
-            ->map(function ($middleware) {
-                return is_string($middleware) ? $middleware : get_class($middleware);
-            })
-            ->values()
-            ->toArray();
-    }
-
-    protected function getParameters($class, $method)
-    {
-        if (!$class || !$method) {
-            return [];
-        }
-
-        try {
-            $reflection = new ReflectionMethod($class, $method);
-            return collect($reflection->getParameters())->map(function (ReflectionParameter $param) {
-                return [
-                    'name' => $param->getName(),
-                    'type' => $param->getType() ? $param->getType()->getName() : 'mixed',
-                ];
-            })->toArray();
-        } catch (\ReflectionException $e) {
-            return [];
-        }
-    }
-
-    protected function getMethodDescription($class, $method)
-    {
-        if (!$class || !$method) {
-            return 'No description available.';
-        }
-
-        try {
-            $reflection = new ReflectionMethod($class, $method);
-            $docComment = $reflection->getDocComment();
-            if ($docComment) {
-                preg_match('/@description\s+(.*)\n/s', $docComment, $matches);
-                return $matches[1] ?? 'No description available.';
+        if ($node instanceof Node\Expr\Array_) {
+            foreach ($node->items as $item) {
+                if ($item->key->value === 'uses') {
+                    return $item->value->value;
+                }
             }
-        } catch (\ReflectionException $e) {
-            // Do nothing
+        } elseif ($node instanceof Node\Scalar\String_) {
+            return $node->value;
         }
+        return 'Closure';
+    }
 
+    protected function getRouteMiddleware(Node\Expr\MethodCall $node)
+    {
+        $middleware = [];
+        $parent = $node->getAttribute('parent');
+        if ($parent instanceof Node\Expr\MethodCall && $parent->name->name === 'middleware') {
+            $middleware = $this->extractMiddleware($parent->args[0]->value);
+        }
+        return $middleware;
+    }
+
+    protected function extractMiddleware($node)
+    {
+        if ($node instanceof Node\Scalar\String_) {
+            return [$node->value];
+        } elseif ($node instanceof Node\Expr\Array_) {
+            return array_map(function($item) {
+                return $item->value->value;
+            }, $node->items);
+        }
+        return [];
+    }
+
+    protected function getActionParameters($action)
+    {
+        if (Str::contains($action, '@')) {
+            list($controller, $method) = explode('@', $action);
+            $controllerFile = app_path('Http/Controllers/' . str_replace('\\', '/', $controller) . '.php');
+            if (File::exists($controllerFile)) {
+                $ast = $this->parseFile($controllerFile);
+                $nodeFinder = new NodeFinder;
+                $methodNode = $nodeFinder->findFirst($ast, function(Node $node) use ($method) {
+                    return $node instanceof Node\Stmt\ClassMethod && $node->name->name === $method;
+                });
+                if ($methodNode) {
+                    return array_map(function($param) {
+                        return [
+                            'name' => $param->var->name,
+                            'type' => $param->type ? $this->getTypeName($param->type) : 'mixed',
+                        ];
+                    }, $methodNode->params);
+                }
+            }
+        }
+        return [];
+    }
+
+    protected function getTypeName($type)
+    {
+        if ($type instanceof Node\Name) {
+            return $type->toString();
+        } elseif ($type instanceof Node\NullableType) {
+            return '?' . $this->getTypeName($type->type);
+        }
+        return (string) $type;
+    }
+
+    protected function getActionDescription($action)
+    {
+        if (Str::contains($action, '@')) {
+            list($controller, $method) = explode('@', $action);
+            $controllerFile = app_path('Http/Controllers/' . str_replace('\\', '/', $controller) . '.php');
+            if (File::exists($controllerFile)) {
+                $ast = $this->parseFile($controllerFile);
+                $nodeFinder = new NodeFinder;
+                $methodNode = $nodeFinder->findFirst($ast, function(Node $node) use ($method) {
+                    return $node instanceof Node\Stmt\ClassMethod && $node->name->name === $method;
+                });
+                if ($methodNode && $methodNode->getDocComment()) {
+                    $docComment = $methodNode->getDocComment()->getText();
+                    if (preg_match('/@description\s+(.*)\n/s', $docComment, $matches)) {
+                        return trim($matches[1]);
+                    }
+                }
+            }
+        }
         return 'No description available.';
     }
 
@@ -146,13 +206,9 @@ class ApiDocumenter
             $output .= "## $fileName\n\n";
 
             foreach ($routes as $route) {
-                $methods = implode(', ', $route['methods']);
-                $output .= "### {$methods} {$route['uri']}\n\n";
+                $output .= "### {$route['method']} {$route['uri']}\n\n";
                 
-                if ($route['handlerClass'] && $route['handlerMethod']) {
-                    $output .= "**Handler:** {$route['handlerClass']}@{$route['handlerMethod']}\n\n";
-                }
-
+                $output .= "**Handler:** {$route['action']}\n\n";
                 $output .= "{$route['description']}\n\n";
 
                 if (!empty($route['parameters'])) {
